d0caf1322eae7e3109abd244f93285c26e7f1303
lewkAa <tihonovtan@gmail.com>
2025-01-23 21:27:02 +0300

    Change data type in service code

--- src/CreditPaymentService.java
+++ src/CreditPaymentService.java
@@ -1,5 +1,5 @@
 public class CreditPaymentService {
-    public double payment(double percent, double amount, double term) { ///  процент по кредиту, сумма, количество лет
+    public int payment(double percent, double amount, double term) { ///  процент по кредиту, сумма, количество лет
         double rPercent = percent / 100;
         double termToMonth = term * 12;
         double monthPercent = rPercent / 12;
--- src/Main.java
+++ src/Main.java
@@ -2,14 +2,15 @@ public class Main {
     public static void main(String[] args) {
         CreditPaymentService service = new CreditPaymentService();
 
-        System.out.printf("При заданных параметрах Ваш ежемесячный платеж составит: ");
-        System.out.println((int)service.payment(9.99,1000000,1)+ " рублей");
+        System.out.print("При заданных параметрах Ваш ежемесячный платеж составит: ");
+        System.out.println(service.payment(9.99,1000000,1)+ " рублей");
 
-        System.out.printf("\nПри заданных параметрах Ваш ежемесячный платеж составит: ");
-        System.out.println((int)service.payment(9.99,1000000,2)+ " рублей");
+        System.out.print("\nПри заданных параметрах Ваш ежемесячный платеж составит: ");
+        System.out.println(service.payment(9.99,1000000,2)+ " рублей");
 
-        System.out.printf("\nПри заданных параметрах Ваш ежемесячный платеж составит: ");
-        System.out.println((int)service.payment(9.99,1000000,3)+ " рублей");
+        System.out.print("\nПри заданных параметрах Ваш ежемесячный платеж составит: ");
+        System.out.println(service.payment(9.99,1000000,3)+ " рублей");
+
 
     }
 }
