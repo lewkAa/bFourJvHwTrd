86b73df70f8cc781e5ba1b8c2c8f161b8c41d036
lewkAa <tihonovtan@gmail.com>
2025-01-23 16:53:35 +0300

    Add code Main.java and service code

---
+++ src/CreditPaymentService.java
@@ -0,0 +1,10 @@
+public class CreditPaymentService {
+    public double payment(double percent, double amount, double term) { ///  процент по кредиту, сумма, количество лет
+        double rPercent = percent / 100;
+        double termToMonth = term * 12;
+        double monthPercent = rPercent / 12;
+        double powedPercent = Math.pow(1 + monthPercent, termToMonth);
+        double annualPayment = amount * ((monthPercent * powedPercent) / (powedPercent - 1));
+        return  (int)annualPayment;
+    }
+}
